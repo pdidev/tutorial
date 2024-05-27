4eea4d7880a819b84280c640a714bc366a510483
yushan.wang <yushan.wang@cea.fr>
2024-05-27 10:36:51 +0200

    update ex4 for writing simu data in sequential mode

--- ex4.c
+++ ex4.c
@@ -177,16 +177,12 @@ int main( int argc, char* argv[] )
 	int ii=0;
 	
 	// share useful configuration bits with PDI
-	PDI_share("ii",         &ii,    PDI_OUT);
-	PDI_reclaim("ii");
 	PDI_share("pcoord",     pcoord, PDI_OUT);
 	PDI_reclaim("pcoord");
 	PDI_share("dsize",      dsize,  PDI_OUT);
 	PDI_reclaim("dsize");
 	PDI_share("psize",      psize,  PDI_OUT);
 	PDI_reclaim("psize");
-	PDI_share("main_field", cur,    PDI_OUT);
-	PDI_reclaim("main_field");
 	
 	// the main loop
 	for (; ii<10; ++ii) {
@@ -207,7 +203,6 @@ int main( int argc, char* argv[] )
 	}
 	// finally share the main field after the main loop body
 	PDI_share("main_field", cur, PDI_OUT);
-	// as well as the loop counter
 	PDI_share("ii",         &ii, PDI_OUT);
 	PDI_reclaim("ii");
 	PDI_reclaim("main_field");
