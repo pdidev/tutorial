94dd96f1ec009fee3bf3129ff09018d9d691547e
yushan.wang <yushan.wang@cea.fr>
2024-05-27 10:35:58 +0200

    update ex3 for writing one time variables

--- ex3.c
+++ ex3.c
@@ -177,24 +177,18 @@ int main( int argc, char* argv[] )
 	int ii=0;
 	
 	// share useful configuration bits with PDI
-	PDI_share("ii",         &ii,    PDI_OUT);
-	PDI_reclaim("ii");
 	PDI_share("pcoord",     pcoord, PDI_OUT);
 	PDI_reclaim("pcoord");
 	PDI_share("dsize",      dsize,  PDI_OUT);
 	PDI_reclaim("dsize");
 	PDI_share("psize",      psize,  PDI_OUT);
 	PDI_reclaim("psize");
-	PDI_share("main_field", cur,    PDI_OUT);
-	PDI_reclaim("main_field");
 	
 	// the main loop
 	for (; ii<10; ++ii) {
 		// share the loop counter & main field at each iteration
-		PDI_share("ii",         &ii, PDI_OUT);
 		PDI_share("main_field", cur, PDI_OUT);
 		PDI_reclaim("main_field");
-		PDI_reclaim("ii");
 		
 		// compute the values for the next iteration
 		iter(cur, next);
@@ -207,9 +201,6 @@ int main( int argc, char* argv[] )
 	}
 	// finally share the main field after the main loop body
 	PDI_share("main_field", cur, PDI_OUT);
-	// as well as the loop counter
-	PDI_share("ii",         &ii, PDI_OUT);
-	PDI_reclaim("ii");
 	PDI_reclaim("main_field");
 	
 	// finalize PDI
