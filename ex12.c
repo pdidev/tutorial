cf1ca57759360d043f3877ae4a1e65713074e069
jacques.morice <jacques.morice@cea.Fr>
2025-01-21 16:39:18 +0100

    fix visibility, fix comment PDI header

--- ex12.c
+++ ex12.c
@@ -28,7 +28,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+
 #include <paraconf.h>
+// load the PDI header
 #include <pdi.h>
 
 /// size of the local data as [HEIGHT, WIDTH] including ghosts & boundary constants
