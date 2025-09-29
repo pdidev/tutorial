5c6800a69906c237f8f4357cddf9229a9ecf59e7
Benoit Martin <bmartin@cea.fr>
2025-09-29 09:23:41 +0200

    update bootstrap

--- spack_env/bootstrap_env.sh
+++ spack_env/bootstrap_env.sh
@@ -4,10 +4,10 @@ SPACK_DIR=/srv/storage/numpexexadi@storage2.grenoble.grid5000.fr/opt/spack
 SPACK_ENV_DIR="spack_env"
 
 if [ -d "$SPACK_ENV_DIR" ]; then
+    echo "preparing spack environment "
     source $SPACK_DIR/share/spack/setup-env.sh
-    spack env activate $SPACK_ENV_DIR
-    spack install -p16
-    spack find
+    spack -e $SPACK_ENV_DIR install -p16
+    spack -e $SPACK_ENV_DIR find
 else
     echo "Please source this file from the repository root."
 fi
