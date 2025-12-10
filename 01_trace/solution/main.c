d892de3b993dd15b42376a016fa269c195897738
yushan wang <yushan.wang@cea.fr>
2025-12-10 11:30:32 +0100

    update solution for 01_trace

--- 01_trace/solution/main.c
+++ 01_trace/solution/main.c
@@ -212,7 +212,7 @@ int main(int argc, char *argv[]) {
 
   // our loop counter so as to be able to use it outside the loop
   int ii = 0;
-  int max_iter = 10;
+  int max_iter = 3;
   
   // the main loop
   for (; ii < max_iter; ++ii) {
